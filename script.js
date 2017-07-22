f668f31da33882e058083e209de89f7a13b7bc8c
ashishsah1000 <ashishsah1000@gmail.com>
2017-07-23 01:58:33 +0530

    Added the achivement section and trekking achivement

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ $(document).ready(function() {
         on: 'hover'
     });
     $('.ui.accordion').accordion();
+    $(window).resize(function() {
+        var width = $(window).width();
+        console.log(width);
+        if (width < 600)
+            $('.to_hide').hide();
+        else
+            $('.to_hide').show();
 
+    });
 });
